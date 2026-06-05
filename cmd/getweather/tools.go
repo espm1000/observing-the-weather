f845400528d82751090dc3692df53bf1d7197e95
espm1000 <esp.m1k@gmail.com>
2026-06-05 15:17:25 -0500

    feat(output): update CSV output handling and add Docker support (#39)
    * feat(csv): update CSV functions to accept directory path for file output
    
    * feat(docker): add Dockerfile for building and running the weather app
    
    * feat(env): add support for WEATHER_REPORT_DIR environment variable
    
    * feat(weather): add chance of precipitation to current weather data

--- cmd/getweather/tools.go
+++ cmd/getweather/tools.go
@@ -1,6 +1,9 @@
 package main
 
+import "log/slog"
+
 func ConvertCelciusToFahrenheit(temp float64) (float64, error) {
+	slog.Info("converting temp to freedom units", "tempC", temp)
 	converted := (temp * 9 / 5) + 32
 	return converted, nil
 }
