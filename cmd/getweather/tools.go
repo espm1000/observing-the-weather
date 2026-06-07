2a251e2de6e0ca63bcce614048835e5f16391ebf
espm1000 <esp.m1k@gmail.com>
2026-06-07 16:38:32 -0500

    feat(logging): enhance logging and reporting for weather data (#41)
    * feat(env): integrate environment variable handling for weather config
    
    * feat(report): implement CSV report generation for current weather data
    
    * feat(logging): remove CSV report generation functions from main.go
    feat(report): add additional column for current timestamp in utc
    
    * feat(logging): add logging functionality with configurable log file
    
    * feat(logging): update logging implementation and remove unused code
    
    * feat(dependencies): add testify and other dependencies to go.mod and go.sum
    
    * feat(logging): enhance logging setup with configurable log directory and level
    
    * feat(build): update TAG in build workflow to a specific version

--- cmd/getweather/tools.go
+++ cmd/getweather/tools.go
@@ -1,9 +1,32 @@
 package main
 
-import "log/slog"
+import (
+	"io"
+	"log/slog"
+	"os"
+	"path"
+)
+
+type LogConfig struct {
+	LogLevel slog.Level
+}
 
 func ConvertCelciusToFahrenheit(temp float64) (float64, error) {
 	slog.Info("converting temp to freedom units", "tempC", temp)
 	converted := (temp * 9 / 5) + 32
 	return converted, nil
+}
+
+func SetLogger(options Environment) (*slog.Logger, error) {
+	slog.Info("setting log config", "log_directory", options.LogDirectory)
+	file, err := os.OpenFile(path.Join(options.LogDirectory, "weatherlog.json"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+	multiwriter := io.MultiWriter(os.Stdout, file)
+	logHandler := slog.NewJSONHandler(multiwriter, &slog.HandlerOptions{
+		Level: options.LogLevel,
+	})
+	logger := slog.New(logHandler)
+	return logger, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/espm1000/observing-the-weather
 
 go 1.25.5
+
+require github.com/caarlos0/env v3.5.0+incompatible
+
+require github.com/stretchr/testify v1.11.1 // indirect
